86ad4225d73bc65c878772ca0d995047aa56bb96
aaa <nigayomogi1105@gmail.com>
2019-06-11 18:14:08 +0900

    First commit

---
+++ input.cpp
@@ -0,0 +1,14 @@
+// input.cpp
+
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int a, b;
+    cin >> a >> b;
+    
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    
+    return 0;
+} 
